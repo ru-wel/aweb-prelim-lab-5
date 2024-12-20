0770b12ead03d3a98d7d20f453efdec98e7897dc
welru <reuelchristian.sundiam04@gmail.com>
2024-12-20 10:25:33 +0800

    added titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './about/about.component';
 import { FeaturedComponent } from './featured/featured.component';
 
 export const routes: Routes = [
-    {path: 'employees', component: EmplistComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'featured', component: FeaturedComponent},
-    {path: '', component: HomeComponent},
+    {path: 'employees', component: EmplistComponent, title: 'Employees'}, // EDIT TITLES
+    {path: 'about', component: AboutComponent, title: 'About'},           // EDIT TITLES
+    {path: 'featured', component: FeaturedComponent, title: 'Featured'},  // EDIT TITLES
+    {path: '', component: HomeComponent, title: 'Home'},                  // EDIT TITLES
 ];
