fbfed5e41766508c88dc182b9a5f25e08fc16d85
welru <reuelchristian.sundiam04@gmail.com>
2024-12-20 10:20:12 +0800

    added components + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmplistComponent } from './emplist/emplist.component';
+import { AboutComponent } from './about/about.component';
+import { FeaturedComponent } from './featured/featured.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'employees', component: EmplistComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'featured', component: FeaturedComponent},
+    {path: '', component: HomeComponent},
+];
---
+++ src/app/emplist/emplist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emplist',
+  standalone: true,
+  imports: [],
+  templateUrl: './emplist.component.html',
+  styleUrl: './emplist.component.css'
+})
+export class EmplistComponent {
+
+}
