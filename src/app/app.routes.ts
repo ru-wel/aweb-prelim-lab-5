b879dc971cbd4e22770690034d6bbf7e769c5079
welru <reuelchristian.sundiam04@gmail.com>
2024-12-21 11:59:26 +0800

    pagenotfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { EmplistComponent } from './emplist/emplist.component';
 import { AboutComponent } from './about/about.component';
 import { FeaturedComponent } from './featured/featured.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
     {path: 'employees', component: EmplistComponent, title: 'EMPLOYEES | SOC'}, // EDIT TITLES
     {path: 'about', component: AboutComponent, title: 'ABOUT | SOC'},           // EDIT TITLES
     {path: 'featured', component: FeaturedComponent, title: 'COURSES | SOC'},  // EDIT TITLES
     {path: '', component: HomeComponent, title: 'HOME | SOC'},                  // EDIT TITLES
+    {path: '**', component: PagenotfoundComponent, title: '404 NOT FOUND' },    
 ];
